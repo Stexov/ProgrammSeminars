7decb8abd72822bd6fbb8de98bcc967826667503
Stexov <stexovdim@gmail.com>
2022-12-15 19:45:44 +0100

    Квадрат

--- Seminar001/ConsoleApp1/Program.cs
+++ Seminar001/ConsoleApp1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает целое число и выдает его квадрат
+Console.Write("Введите целое число: ");
+//int number = 8;
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Квадрат числа = {number * number}");
+
+
+
