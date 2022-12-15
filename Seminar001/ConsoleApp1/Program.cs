abe769e228b9835ca1a863f9b8544bb4a34049c0
Stexov <119614834+Stexov@users.noreply.github.com>
2022-12-15 22:41:53 +0100

    Delete Seminar001/ConsoleApp1 directory

--- Seminar001/ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Принимает целое число и выдает его квадрат
-Console.Write("Введите целое число: ");
-//int number = 8;
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Квадрат числа = {number * number}");
-
-
-
