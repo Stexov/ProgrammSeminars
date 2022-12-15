df1fe41b3213fa9fa18ec6a4ed1c00e07eca85c9
Stexov <stexovdim@gmail.com>
2022-12-15 22:33:47 +0100

    Решил задачу2

---
+++ Seminar001/Task_002/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write("Максимум = ");
+    Console.WriteLine(number1);
+    Console.Write("Минимум = ");
+    Console.WriteLine(number2);
+}
+if (number1 < number2)
+{
+    Console.Write("Максимум = ");
+    Console.WriteLine(number2);
+    Console.Write("Минимум = ");
+    Console.WriteLine(number1);
+}
+if (number1 == number2)
+{
+        Console.WriteLine("Числа равны");
+}
