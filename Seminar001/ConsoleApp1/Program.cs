726cbe27359c19eb1224dc889422f3317d98e772
Stexov <stexovdim@gmail.com>
2022-12-16 11:36:15 +0100

    Удалил папку

--- Seminar001/ConsoleApp1/Program.cs
+++ Seminar001/ConsoleApp1/Program.cs
@@ -1,8 +1,17 @@
-﻿// Принимает целое число и выдает его квадрат
-Console.Write("Введите целое число: ");
-//int number = 8;
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Квадрат числа = {number * number}");
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number1 / number2;
+if (result == number2)
+{
+    Console.WriteLine("Первое число есть квадрат второго");
+}
+else
+{
+    Console.WriteLine("Первое число не квадрат второго");
+}
+
 
 
 
