ef52609422364a33425fe69e63a486dac25b9513
Stexov <stexovdim@gmail.com>
2022-12-21 16:12:40 +0100

    Выполнил домашнее задание

---
+++ Seminars002/Task_015/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Ввдетите цифру обозначающую день недели: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+if (numbers > 7)
+{
+    Console.WriteLine("Введенное число не удолетворяет диапазону");
+//    break;
+}
+if (numbers < 6)
+{
+    Console.WriteLine("Этот день рабочий");
+}
+else
+{
+    Console.WriteLine("Этот день выходной");
+}
