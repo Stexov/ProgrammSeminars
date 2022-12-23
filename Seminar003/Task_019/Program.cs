56e550883255b4309f6e1fab80f60c43a457d235
Stexov <stexovdim@gmail.com>
2022-12-23 22:00:49 +0100

    Выполнил домашнее задание

---
+++ Seminar003/Task_019/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число");
+int sat = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+if (sat > 99999)
+{
+    Console.WriteLine("Введенное число не удолетворяет условию");
+    return;
+}
+int[] array = new int[5];
+for (int i = 5; i > 0; i--)
+{
+    num = sat % 10;
+    array[i-1] = num;
+    sat = sat / 10;
+}
+if (array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine("Введенное число палиндромом");
+}
+else
+{
+    Console.WriteLine("Введенное число не палиндромом");
+}
---
+++ Seminar003/Task_023/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N: ");
+int result = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= result; i++)
+{
+    Console.WriteLine($"Куб числа {i} = {i*i*i}");
+}
