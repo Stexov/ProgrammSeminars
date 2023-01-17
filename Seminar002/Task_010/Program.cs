d3b08ea65133e2fccf5ffca08ba6a5063a66a8a6
Stexov <stexovdim@gmail.com>
2023-01-17 18:18:49 +0100

    переименовл папку

---
+++ Seminar002/Task_010/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Ввдетите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+if (numbers < 1000)
+{
+    numbers = numbers % 100;
+    Console.WriteLine($"числа: {numbers}");
+    numbers = numbers / 10;
+    if (numbers < 0)
+    {
+        numbers = numbers * -1;
+    }
+    Console.WriteLine($"Вторая цифра числа: {numbers}");
+}
+else
+{
+    Console.WriteLine("Введенное число не является трехзначным"); 
+}
---
+++ Seminar002/Task_013/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Ввдетите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+if (numbers < 0)
+{
+    numbers = numbers * -1;
+}
+if (numbers < 100)
+{
+    Console.WriteLine("Введенное число не является трехзначным"); 
+}
+while (numbers > 999)
+{
+    numbers = numbers / 10;
+}
+numbers = numbers % 10;
+Console.WriteLine($"Искомое число = {numbers}");
